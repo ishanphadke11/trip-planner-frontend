df9c8eb095ae38bcdddb0db3402c72ed7562f688
Ishan Phadke <ishan.phadke@netscout.com>
2025-08-27 10:29:39 -0400

    Created .env for firebase

--- frontend2/src/firebase.js
+++ frontend2/src/firebase.js
@@ -10,15 +10,17 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDDbw3BPyrFDUE08zbJYM0Vn41VXDuQIiU",
-  authDomain: "tripmaker-c1e73.firebaseapp.com",
-  projectId: "tripmaker-c1e73",
-  storageBucket: "tripmaker-c1e73.firebasestorage.app",
-  messagingSenderId: "137274651666",
-  appId: "1:137274651666:web:b8d3ffa6cd6c82bce8d4cb",
-  measurementId: "G-EMFH2SDDBH"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
